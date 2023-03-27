e515446ef89c84ee9ce9d6c60b019543e9765cac
eduardonk9999 <thiago.novak7@gmail.com>
2023-03-27 17:03:44 -0300

    animacao

--- AnimacaoTela.java
+++ AnimacaoTela.java
@@ -1,5 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class AnimacaoTela extends JFrame{
     private JPanel janela;
@@ -23,5 +25,37 @@ public class AnimacaoTela extends JFrame{
 
             }
         }
+    }
+
+    public AnimacaoTela() {
+        janela = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+            // Limpando os desenhos anteriores
+            g.setColor(Color.WHITE);
+            g.fillRect(0, 0, janela.getWidth(),
+            janela.getHeight());
+            g.setColor(Color.BLUE);
+            g.drawLine(0, 240 + ct, 640, 240 + ct);
+            g.drawRect(10, 25 + ct, 20, 20);
+            g.drawOval(30 + ct, 20, 40, 30);
+            g.setColor(Color.YELLOW);
+            g.drawLine(320 - ct, 0, 320 - ct, 480);
+            g.fillRect(110, 125, 120 - ct, 120 - ct);
+            g.fillOval(230, 220, 240 + ct, 230);
+            g.setColor(Color.RED);
+            g.drawString("Eu seria um ótimo Score! " + ct, 5, 10);
+        }
+
+        };
+        getContentPane().add(janela);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setVisible(true);
+        janela.repaint();
+    }
+    public static void main(String[] args) {
+        AnimacaoTela anima = new AnimacaoTela();
+        anima.iniciaAnimacao();
     }
 }
